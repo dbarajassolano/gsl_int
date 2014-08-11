cf2ad75fe54df0aa7550f8f7f7fbc220cf0ba718
David Barajas-Solano <dbarajassolano@gmail.com>
2014-08-10 23:11:47 -0700

    Added copyright notices

--- include/gsl_rvgen.h
+++ include/gsl_rvgen.h
@@ -1,3 +1,24 @@
+/*  GSL-INT -- Some useful interfaces for GSL-INT
+    Copyright (C) 2014 David Barajas-Solano
+
+    This program is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    This project is based on code from the Dieharder project Copyright
+    (C) 2003 by Robert G. Brown <rgb@phy.duke.edu>
+    License: <http://www.gnu.org/licenses/> GPL version 2 or higher
+*/
+
 #ifndef GSL_RVGEN_H
 #define GSL_RVGEN_H
 
